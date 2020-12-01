92d08e4d6d63a065dad5f3a778846b60f1c79746
MartienJun <martienjunaedi123@gmail.com>
2020-12-01 15:44:27 +0700

    add landing parallax effect
    parallax effect using Rellax JS (https://dixonandmoe.com/rellax/)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-$(window).on("load", function(){
-    $("")
-})
+// $(window).on("load", function(){
+//     // console.log("ok");
+//     // $(".landing img").fadeIn(1800);
+//     $(".landing img").animate({
+//         top: "220px"
+//     })
+
+//     //$(".landing img").
+//     $(".landing h4").fadeIn(1800);
+//     $(".landing .text1").fadeIn(1800);
+//     $(".landing .text2").fadeIn(1800);
+// });
